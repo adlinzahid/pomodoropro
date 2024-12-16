a98c572c8b79b4df2fb7841107e9fd1c947ebb9d
adlinzahid <zahidadlin@gmail.com>
2024-12-17 02:40:36 +0800

    update homepage to display user's current points and completed tasks from yesterday

--- lib/screens/Homepage/homepage.dart
+++ lib/screens/Homepage/homepage.dart
@@ -138,144 +138,317 @@ class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   final Tasksdata tasksdata = Tasksdata();
+  final int currentPoints = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: GestureDetector(
-            onTap: () {
-              log('Box Clicked');
-            },
-            child: Container(
-              width: double.infinity,
-              height: 200.0,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 101, 181, 103),
-                borderRadius: BorderRadius.circular(8.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    spreadRadius: 3,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '0 Days',
-                    style: GoogleFonts.quicksand(
-                      color: Colors.white,
-                      fontSize: 45.0,
+    return SingleChildScrollView(
+      // Makes the page scrollable
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // First Section: Streak Box
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: GestureDetector(
+              onTap: () {
+                log('Box Clicked');
+              },
+              child: Container(
+                width: double.infinity,
+                height: 200.0,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 101, 181, 103),
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
                     ),
-                  ),
-                  const SizedBox(height: 10.0),
-                  Text(
-                    'Current Streak',
-                    style: GoogleFonts.quicksand(
-                      color: Colors.white,
-                      fontSize: 30.0,
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '0 Days',
+                      style: GoogleFonts.quicksand(
+                        color: Colors.white,
+                        fontSize: 45.0,
+                      ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 10.0),
+                    Text(
+                      'Current Streak',
+                      style: GoogleFonts.quicksand(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 16.0, top: 20.0),
-          child: Row(
-            children: [
-              Text(
-                "Today's Goals",
-                style: GoogleFonts.quicksand(
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.amber[700]),
-              ),
-              IconButton(
-                icon: Icon(Icons.add_circle_outlined,
-                    size: 35.0, color: Colors.lightGreen[300]),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => TodoList(),
-                    ),
-                  );
-                },
-              ),
-            ],
+          // Second Section: Today's Goals
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, top: 20.0),
+            child: Row(
+              children: [
+                Text(
+                  "Today's Goals",
+                  style: GoogleFonts.quicksand(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.amber[700]),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add_circle_outlined,
+                      size: 35.0, color: Colors.lightGreen[300]),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TodoList(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
-        // display today's goals here
-        Expanded(
+          // Display Today's Goals
+          SizedBox(
+            // Set a height constraint for the list
             child: StreamBuilder<List<Map<String, dynamic>>>(
-          stream: tasksdata.getTodayTasks(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            }
+              stream: tasksdata.getTodayTasks(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
 
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+                final tasks = snapshot.data;
 
-            final tasks = snapshot.data;
-
-            return ListView.builder(
-              itemCount: tasks?.length,
-              itemBuilder: (context, index) {
-                final task = tasks?[index];
-                return Card(
-                  color: Colors.blue[50], // card colour
-                  margin: const EdgeInsets.symmetric(
-                      horizontal: 12.0, vertical: 6.0),
-                  child: ListTile(
-                    title: Text(task?['Name'],
-                        style: GoogleFonts.quicksand(
-                            fontSize: 15.0, fontWeight: FontWeight.w600)),
-                    subtitle: Text(
-                      task?['Notes'],
-                      style: GoogleFonts.quicksand(
-                          fontSize: 12.0, fontWeight: FontWeight.w500),
-                    ),
-                    trailing: Text(
-                      "Due: ${_formatTime(task?['time'])}",
-                      style: GoogleFonts.quicksand(
-                          color: Colors.indigo[900],
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics:
+                      NeverScrollableScrollPhysics(), // Disable nested scrolling
+                  itemCount: tasks?.length,
+                  itemBuilder: (context, index) {
+                    final task = tasks?[index];
+                    return Card(
+                      color: Colors.blue[50],
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 12.0, vertical: 6.0),
+                      child: ListTile(
+                        title: Text(task?['Name'] ?? '',
+                            style: GoogleFonts.quicksand(
+                                fontSize: 15.0, fontWeight: FontWeight.w600)),
+                        subtitle: Text(
+                          task?['Notes'] ?? '',
+                          style: GoogleFonts.quicksand(
+                              fontSize: 12.0, fontWeight: FontWeight.w500),
+                        ),
+                        trailing: Text(
+                          "Due: ${_formatTime(task?['time'])}",
+                          style: GoogleFonts.quicksand(
+                              color: Colors.indigo[900],
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    );
+                  },
                 );
               },
-            );
-          },
-        )),
-      ],
+            ),
+          ),
+
+          // Third Section: Your Progress
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, top: 20.0, right: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Your Progress",
+                      style: GoogleFonts.quicksand(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.amber[700],
+                      ),
+                    ),
+                    const SizedBox(height: 4.0),
+                    Text(
+                      "Yesterday",
+                      style: GoogleFonts.quicksand(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+
+                // Fetching and displaying current points using FutureBuilder
+                FutureBuilder<int>(
+                  future: tasksdata.getUserPoints(), // Fetch current points
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator(); // Loading spinner
+                    }
+
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    }
+
+                    final currentPoints = snapshot.data ?? 0;
+
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "$currentPoints Points", // Display points dynamically
+                          style: GoogleFonts.quicksand(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
+                        ),
+                        const SizedBox(height: 4.0),
+                        Text(
+                          _getYesterdayDate(),
+                          style: GoogleFonts.quicksand(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          // Display Yesterday's Completed Tasks
+          SizedBox(
+            child: StreamBuilder<List<Map<String, dynamic>>>(
+              stream: tasksdata.getCompletedTasks(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final tasks = snapshot.data;
+
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics:
+                      NeverScrollableScrollPhysics(), // Disable nested scrolling
+                  itemCount: tasks?.length,
+                  itemBuilder: (context, index) {
+                    final task = tasks?[index];
+                    return Card(
+                      color: Colors.blue[50],
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 12.0, vertical: 6.0),
+                      child: ListTile(
+                        title: Text(task?['Name'] ?? '',
+                            style: GoogleFonts.quicksand(
+                                fontSize: 15.0, fontWeight: FontWeight.w600)),
+                        subtitle: Text(
+                          task?['Notes'] ?? '',
+                          style: GoogleFonts.quicksand(
+                              fontSize: 12.0, fontWeight: FontWeight.w500),
+                        ),
+                        trailing: Text(
+                          "Completed: at ${_formatTime(task?['time'])}",
+                          style: GoogleFonts.quicksand(
+                              color: Colors.indigo[900],
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-// format to 12-hour time with AM/PM
-  _formatTime(task) {
+  // Format time utility
+  String _formatTime(task) {
     try {
       final time = task.toDate();
       final hour = time.hour > 12 ? time.hour - 12 : time.hour;
       final minute = time.minute.toString().padLeft(2, '0');
-      final period = time.hour > 12 ? 'PM' : 'AM';
+      final period = time.hour >= 12 ? 'PM' : 'AM';
       return '$hour:$minute $period';
     } catch (e) {
       return 'Invalid Time';
+    }
+  }
+
+  //function to get yesterday's date
+  String _getYesterdayDate() {
+    final DateTime now = DateTime.now();
+    final DateTime yesterday = now.subtract(const Duration(days: 1));
+    return "${yesterday.day} ${_getMonthName(yesterday.month)} ${yesterday.year}";
+  }
+
+  //helper function to get month name
+  String _getMonthName(int month) {
+    switch (month) {
+      case 1:
+        return 'January';
+      case 2:
+        return 'February';
+      case 3:
+        return 'March';
+      case 4:
+        return 'April';
+      case 5:
+        return 'May';
+      case 6:
+        return 'June';
+      case 7:
+        return 'July';
+      case 8:
+        return 'August';
+      case 9:
+        return 'September';
+      case 10:
+        return 'October';
+      case 11:
+        return 'November';
+      case 12:
+        return 'December';
+      default:
+        return 'Invalid Month';
     }
   }
 }
