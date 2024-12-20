2d132bc38ba9339a5a37dee87fa8ab0d69669bbc
adlinzahid <zahidadlin@gmail.com>
2024-12-20 19:30:09 +0800

    new ui look for groupscreen

--- lib/screens/GroupCollab/groupscreen.dart
+++ lib/screens/GroupCollab/groupscreen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'create_group.dart'; // Import the CreateGroupPage file
+import 'join_group.dart'; // Import the JoinGroupPage file
 
-class CreateGroupPage extends StatefulWidget {
-  const CreateGroupPage({super.key});
-  @override
-
- State<CreateGroupPage> createState() => _CreateGroupPageState();
-  
+void main() {
+  runApp(const GroupCollaborationApp());
 }
-class _CreateGroupPageState extends State<CreateGroupPage> {
+
+class GroupCollaborationApp extends StatelessWidget {
+  const GroupCollaborationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const GroupCollaborationPage(),
+    );
+  }
+}
+
+class GroupCollaborationPage extends StatelessWidget {
+  const GroupCollaborationPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +30,6 @@ class _CreateGroupPageState extends State<CreateGroupPage> {
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios, color: Colors.black87),
-          onPressed: () {
-            Navigator.pop(context); // Go back to GroupScreen
-          },
-        ),
         title: Text(
           'Group Collaboration',
           style: GoogleFonts.quicksand(
@@ -32,123 +39,86 @@ class _CreateGroupPageState extends State<CreateGroupPage> {
           ),
         ),
       ),
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.85, // Medium fit width
-          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-          decoration: BoxDecoration(
-            color: Colors.green[400],
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 4,
-                offset: const Offset(0, 4),
-              ),
-            ],
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+          _buildButton(
+            context,
+            label: 'Create Group',
+            icon: Icons.add,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const CreateGroupPage(),
+                ),
+              );
+            },
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Username Input
-              Text(
-                'Username',
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.white,
+          const SizedBox(height: 15),
+          _buildButton(
+            context,
+            label: 'Join Group',
+            icon: Icons.link,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const JoinGroupPage(),
                 ),
-              ),
-              const SizedBox(height: 10),
-              _buildInputField(),
-              const SizedBox(height: 20),
-              // Code Input
-              Text(
-                'Code',
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              _buildInputField(),
-              const SizedBox(height: 10),
-              // Instructional Text
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  '• use this code to create the group',
-                  style: GoogleFonts.poppins(
-                    fontSize: 12,
-                    color: Colors.black87,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              // "CREATE" Text Button
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const GroupTaskCreatedPage(),
-                    ),
-                  );
-                },
-                child: Text(
-                  'CREATE',
-                  style: GoogleFonts.poppins(
-                    fontSize: 16,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    decoration: TextDecoration.underline,
-                    decorationColor: Colors.white,
-                  ),
-                ),
-              ),
-            ],
+              );
+            },
           ),
-        ),
-      ),
-    );
-  }
-  // Input Field Widget
-  Widget _buildInputField() {
-    return Container(
-      height: 40,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 2,
-            offset: const Offset(0, 2),
+          const SizedBox(height: 50),
+          const Center(
+            child: Text(
+              'No Groups Created',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
+              ),
+            ),
           ),
         ],
       ),
-      child: TextField(
-        style: GoogleFonts.poppins(color: Colors.black87),
-        decoration: const InputDecoration(
-          contentPadding: EdgeInsets.symmetric(horizontal: 10),
-          border: InputBorder.none,
-        ),
-      ),
     );
   }
-}
-// Dummy Group Task Created Page
-class GroupTaskCreatedPage extends StatelessWidget {
-  const GroupTaskCreatedPage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Group Task Created'),
+
+  // Reusable button widget
+  Widget _buildButton(BuildContext context,
+      {required String label,
+      required IconData icon,
+      required VoidCallback onPressed}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16.0),
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.green[400],
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: const Offset(0, 4),
+            blurRadius: 4,
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('This is the Group Task Created Page'),
+      child: TextButton.icon(
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          color: Colors.white,
+          size: 28,
+        ),
+        label: Text(
+          label,
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
       ),
     );
   }
