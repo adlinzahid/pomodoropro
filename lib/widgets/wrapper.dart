f2ea80caf40b664b96ae132a0c9811814a93bb6f
adlinzahid <zahidadlin@gmail.com>
2024-12-29 03:07:17 +0800

    refactor Wrapper widget to include Scaffold for improved layout structure

--- lib/widgets/wrapper.dart
+++ lib/widgets/wrapper.dart
@@ -1,26 +1,25 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import '../screens/Homepage/homepage.dart';
-import '../screens/Homepage/splashscreen.dart';
-
+import 'package:pomodororpo/screens/Homepage/homepage.dart';
+import 'package:pomodororpo/screens/Homepage/splashscreen.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const SplashScreen();
-        } else if (snapshot.hasData) {
-          return const MyHomePage();
-        } else {
-          return const SplashScreen();
-        }
-      },
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            } else if (snapshot.hasData) { 
+              return const MyHomePage();
+            } else {
+              return const SplashScreen();
+            }
+          }),
     );
   }
 }
