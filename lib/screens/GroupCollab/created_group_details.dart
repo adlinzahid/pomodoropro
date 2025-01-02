92f71c9fa73e4108d7280d693ab7ea526c16d71c
adlinzahid <zahidadlin@gmail.com>
2025-01-02 18:50:18 +0800

    add due date display to group details and remove unused member fetching logic

--- lib/screens/GroupCollab/created_group_details.dart
+++ lib/screens/GroupCollab/created_group_details.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class GroupDetailsScreen extends StatefulWidget {
   final String uniqueCode; // Accept the unique code as a parameter
@@ -82,6 +82,11 @@ class _GroupDetailsScreenState extends State<GroupDetailsScreen> {
 
   // Build group details card
   Widget _buildGroupDetailsCard(Map<String, dynamic> groupData) {
+    Timestamp? dueDateTimestamp = groupData['dueDate']; // Get the due date
+    String formattedDueDate = dueDateTimestamp != null
+        ? DateFormat.yMMMd().format(dueDateTimestamp.toDate())
+        : 'No Due Date';
+
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 20),
       elevation: 5,
@@ -108,74 +113,22 @@ class _GroupDetailsScreenState extends State<GroupDetailsScreen> {
             ),
             const SizedBox(height: 10),
             Text(
-              groupData['description'] ?? 'Description',
+              "Description: ${groupData['description'] ?? 'Description'}",
               style: GoogleFonts.quicksand(
                 fontSize: 16,
               ),
             ),
+            //show the due date of the group project from firestore groups collection
             const SizedBox(height: 10),
             Text(
-              'Members:',
+              "Due Date: $formattedDueDate",
               style: GoogleFonts.quicksand(
                 fontSize: 16,
-                fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 5),
-            if (groupData['members'] != null)
-              FutureBuilder<List<String>>(
-                future: _fetchMemberUsernames(groupData['members']),
-                builder: (context, memberSnapshot) {
-                  if (memberSnapshot.connectionState ==
-                      ConnectionState.waiting) {
-                    return const CircularProgressIndicator();
-                  } else if (memberSnapshot.hasError) {
-                    return Text('Error: ${memberSnapshot.error}');
-                  } else {
-                    final memberUsernames = memberSnapshot.data;
-                    return Column(
-                      children: memberUsernames?.map((username) {
-                            return Text(
-                              FirebaseAuth.instance.currentUser?.displayName ==
-                                      username
-                                  ? '$username (You)'
-                                  : username,
-                              style: GoogleFonts.quicksand(
-                                fontSize: 16,
-                              ),
-                            );
-                          }).toList() ??
-                          [],
-                    );
-                  }
-                },
-              ),
           ],
         ),
       ),
     );
-  }
-
-  Future<List<String>> _fetchMemberUsernames(List<dynamic> memberIds) async {
-    List<String> usernames = [];
-
-    for (var memberId in memberIds) {
-      if (memberId == FirebaseAuth.instance.currentUser?.uid) {
-        // If the member is the current user, use their displayName directly
-        usernames.add(
-            FirebaseAuth.instance.currentUser?.displayName ?? 'No Username');
-      } else {
-        // Fetch the username of other members from Firestore
-        final userDoc =
-            await _firestore.collection('users').doc(memberId).get();
-        if (userDoc.exists) {
-          usernames.add(userDoc.data()?['username'] ?? 'No Username');
-        } else {
-          usernames.add('No Username');
-        }
-      }
-    }
-
-    return usernames;
   }
 }
